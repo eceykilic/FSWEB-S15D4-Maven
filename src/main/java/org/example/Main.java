44beed39d11a5a890b52cbb6817a589932b2787b
eceykilic <eceykilic@gmail.com>
2024-04-04 22:03:24 +0300

    solutions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,38 @@
 package org.example;
 
 
+import java.util.LinkedList;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+    }
+
+    public static boolean checkForPalindrome(String text) {
+        String cleanText = text.toLowerCase(Locale.ENGLISH).replaceAll("[^a-zA-Z]", "");
+        int n = cleanText.length();
+        for (int i = 0; i <= n / 2; i++) {
+            if (cleanText.charAt(i) != cleanText.charAt(n - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static String convertDecimalToBinary(int n){
+        LinkedList<Integer> binaryList = new LinkedList<>();
+        while(n > 0){
+            int r = n % 2;
+            binaryList.addFirst(r);
+            n /= 2;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int number : binaryList){
+            sb.append(number);
+        }
+        return  sb.toString();
     }
 
 }
